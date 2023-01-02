eb26557b08226a453b79445aebd2b036f35b9323
Dlnuzmc <77119185+Dlnuzmc@users.noreply.github.com>
2023-01-02 15:53:01 +0300

    SELECT within SELECT

--- 4_SELECT_within_SELECT.sql
+++ 4_SELECT_within_SELECT.sql
@@ -7,27 +7,27 @@ SELECT name FROM world
   SELECT population FROM world
    WHERE name = 'Russia');
 
--- # 2. List the name and continent of countries in the continents containing 'Belize', 'Belgium'.
-SELECT name, continent FROM world
- WHERE continent IN (
-  SELECT continent FROM world
-   WHERE name IN ('Belize', 'Belgium'));
-
--- # 3. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+-- # 2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
 SELECT name FROM world
  WHERE continent = 'Europe'
   AND gdp/population > (
    SELECT gdp/population FROM world
     WHERE name = 'United Kingdom');
 
--- # 4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+-- # 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+SELECT name, continent FROM world
+ WHERE continent IN (
+  SELECT continent FROM world
+   WHERE name IN ('Argentina', 'Australia'));
+
+-- # 4. Which country has a population that is more than United Kingdom but less than Germany? Show the name and the population.
 SELECT name, population FROM world
  WHERE population > (
    SELECT population FROM world
-    WHERE name = 'Canada')
+    WHERE name = 'United Kingdom')
   AND population < (
    SELECT population FROM world
-    WHERE name = 'Poland');
+    WHERE name = 'Germany');
 
 -- # 5. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
 SELECT name, CONCAT(CAST(100*ROUND((population / (SELECT population FROM world WHERE name ='Germany')), 2) AS INT), '%')
